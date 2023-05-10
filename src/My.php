56c95a2961c954556b3c7876406096b3307b9e65
Jean-Christian Denis <contact@jcdenis.fr>
2023-05-10 10:50:36 +0200

    no more needed

--- src/My.php
+++ src/My.php
@@ -24,9 +24,6 @@ class My
     /** @var    string  Mailer name */
     public const X_MAILER = 'Dotclear';
 
-    /** @var    string  This module required php version */
-    public const PHP_MIN = '7.4';
-
     /**
      * This module id.
      */
@@ -51,13 +48,5 @@ class My
     public static function path(): string
     {
         return dirname(__DIR__);
-    }
-
-    /**
-     * Check this module PHP version compliant.
-     */
-    public static function phpCompliant(): bool
-    {
-        return version_compare(phpversion(), self::PHP_MIN, '>=');
     }
 }
